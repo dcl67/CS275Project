73340e85903d645e9276713d5c12110b8d6072ac
dcl67 <dcl67@drexel.edu>
2017-08-30 09:00:57 -0400

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -1,101 +1,111 @@
-var curTurn = 'X';
-var boxes = ["0","0","0","0","0","0","0","0","0"];
-var turnCount = 0;
-var over = false;
+$(function() {
 
-function place(){
-    if(!over) {
-        if (isValid(event.target)) {
-            turnCount += 1;
-            event.target.value = curTurn;
-            logBox(event.target.id,curTurn);
-            checkGameState();
-            if (!over) {
-                advanceTurn();
+    var player='X';
+    var table = $('table');
+    var messages = $('.messages');
+    var turn = $('.turn');
+    displayNextPlayer(turn, player);
+
+    $('td').click(function() {
+        td = $(this);
+        var state = getState(td);
+        if(!state) {
+            var pattern = definePatternForCurrentPlayer(player);
+            changeState(td, pattern);
+            if(checkIfPlayerWon(table, pattern)) {
+                addScore(player);
+                messages.html('Player '+player+' has won.');
+                turn.html('');
+            } else {
+                player = setNextPlayer(player);
+                displayNextPlayer(turn, player);
             }
+        } else {
+            messages.html('This box is already checked.');
         }
-    }
-}
+    });
 
-function logBox(bId,turn) {
-    switch(bId) {
-        case "tl":
-            boxes[0] = turn;
-            break;
-        case "tm":
-            boxes[1] = turn;
-            break;
-        case "tr":
-            boxes[2] = turn;
-            break;
-        case "ml":
-            boxes[3] = turn;
-            break;
-        case "mm":
-            boxes[4] = turn;
-            break;
-        case "mr":
-            boxes[5] = turn;
-            break;
-        case "bl":
-            boxes[6] = turn;
-            break;
-        case "bm":
-            boxes[7] = turn;
-            break;
-        case "br":
-            boxes[8] = turn;
-            break;
-    }
-}
+    $('.reset').click(function() {
+        player='X';
+        messages.html('');
+        reset(table);
+        displayNextPlayer(turn, player);
+    });
 
-function checkGameState() {
-    if (_checkHorizantal(boxes) || _checkVetical(boxes) || _checkDiaganol(boxes)) {
-        document.getElementById("turn").innerHTML = curTurn + " wins!";
-        over = true;
-    } else if (turnCount >= 9) {
-        document.getElementById("turn").innerHTML = "It is a tie!";
-        over = true;
-    }
+});
 
-}
-
-function _checkHorizantal() {
-    return (boxes[0] == boxes[1] && boxes[1] == boxes[2] && boxes[0] != "0") || (boxes[3] == boxes[4] && boxes[4] == boxes[5] && boxes[3] != "0") || (boxes[6] == boxes[7] && boxes[7] == boxes[8] && boxes[6] != "0");
-}
-
-function _checkVetical() {
-    return (boxes[0] == boxes[3] && boxes[6] == boxes[3] && boxes[0] != "0") || (boxes[1] == boxes[4] && boxes[4] == boxes[7] && boxes[1] != "0") || (boxes[2] == boxes[5] && boxes[5] == boxes[8] && boxes[2] != "0");
-}
-
-function _checkDiaganol() {
-    return (boxes[0] == boxes[4] && boxes[4] == boxes[8] && boxes[0] != "0") || (boxes[2] == boxes[4] && boxes[4] == boxes[6] && boxes[4] != "0");
-}
-
-function isValid(button) {
-    if (button.value == ""){
-        return true;
+function getState(td) {
+    if(td.hasClass('cross') || td.hasClass('circle')) {
+        return 1;
     } else {
-        return false;
+        return 0;
     }
 }
 
-function advanceTurn() {
-    if (curTurn == "X"){
-        curTurn = "O";
-    } else if (curTurn == "O") {
-        curTurn = "X";
-    }
-    document.getElementById("turn").innerHTML = "Current Turn: " + curTurn;
+function changeState(td, pattern) {
+    return td.addClass(pattern);
 }
 
-function resetGame() {
-    curTurn = 'X';
-    boxes = ["0","0","0","0","0","0","0","0","0"];
-    turnCount = 0;
-    over = false;
-    document.getElementById("turn").innerHTML = "Current Turn: " + curTurn;
-    $(".button_field").each(function() {
-        $(this).attr("value","");
+function definePatternForCurrentPlayer(player) {
+    if(player == 'X') {
+        return 'cross';
+    } else {
+        return 'circle';
+    }
+}
+
+function setNextPlayer(player) {
+    if(player == 'X') {
+        return player = 'O';
+    } else {
+        return player='X';
+    }
+}
+
+function displayNextPlayer(turn, player) {
+    turn.html('Player turn : '+player);
+}
+
+function checkIfPlayerWon(table, pattern) {
+    var won = 0;
+    if(table.find('.item1').hasClass(pattern) && table.find('.item2').hasClass(pattern) && table.find('.item3').hasClass(pattern)) {
+        won = 1;
+    } else if (table.find('.item1').hasClass(pattern) && table.find('.item4').hasClass(pattern) && table.find('.item7').hasClass(pattern)) {
+        won = 1;
+    } else if (table.find('.item1').hasClass(pattern) && table.find('.item5').hasClass(pattern) && table.find('.item9').hasClass(pattern)) {
+        won = 1;
+    } else if (table.find('.item4').hasClass(pattern) && table.find('.item5').hasClass(pattern) && table.find('.item6').hasClass(pattern)) {
+        won = 1;
+    } else if (table.find('.item7').hasClass(pattern) && table.find('.item8').hasClass(pattern) && table.find('.item9').hasClass(pattern)) {
+        won = 1;
+    } else if (table.find('.item2').hasClass(pattern) && table.find('.item5').hasClass(pattern) && table.find('.item8').hasClass(pattern)) {
+        won = 1;
+    } else if (table.find('.item3').hasClass(pattern) && table.find('.item6').hasClass(pattern) && table.find('.item9').hasClass(pattern)) {
+        won = 1;
+    } else if (table.find('.item3').hasClass(pattern) && table.find('.item5').hasClass(pattern) && table.find('.item7').hasClass(pattern)) {
+        won = 1;
+    }
+    return won;
+}
+
+function reset(table) {
+    table.find('td').each(function() {
+        $(this).removeClass('circle').removeClass('cross');
+    });
+}
+
+function addScore(player){
+    var URL = 'http://localhost:8080/addscore';
+    console.log("Winner: "+player);
+    $.ajax({
+        type: 'POST',
+        url: URL,
+        data: {str:player},
+        success: function(msg){
+            console.log(msg); //log success message
+        },
+        error: function(xhr, ajaxOptions, thrownError){
+            alert('client.js: Could not add score!');
+        }
     });
 }
